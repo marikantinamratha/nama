0c28b6e1bbc1efacac2c2d697f526248db8d9875
marikantinamratha <marikantinamratha@gmail.com>
2026-09-20 11:29:46 +0530

    remove redundant comment explaining strong number logic

--- strong.py
+++ strong.py
@@ -14,4 +14,4 @@ if sum==num:
     print('strong')
 else:
     print('not strong')
-    
+#this program is used to check whether the given number is strong or not. A strong number is a number in which the sum of the factorial of digits is equal to the original number. For example, 145 is a strong number because 1! + 4! + 5! = 145.
